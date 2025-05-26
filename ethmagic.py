47774c07dc12ff5929528b36c1b0f8627a836ff8
Terremoto21 <marioscarpa989@gmail.com>
2025-05-26 20:05:55 +0200

    ethmagic.py

--- ethmagic.py
+++ ethmagic.py
@@ -13,7 +13,7 @@ def Main():
     parser.add_argument('-f', '--file', dest="filenameEth", required=True,
                         help="Ethereum Rich Address File With Type Format .TXT [Example: -f eth5.txt]")
     parser.add_argument('-v', '--view', dest="ViewPrint", required=True,
-                        help="Print After Generated This Number Print And Report")
+                        help="Print After Generated This Number Print AnReport")
     parser.add_argument('-n', '--thread', dest="ThreadCount", required=True,
                         help="Total Thread Number (Total Core CPU)")
 
@@ -37,16 +37,16 @@ def Main():
         ctypes.windll.kernel32.SetConsoleTitleW(f"MATCH:{fu} SCAN:{z}")
         mnemonic = Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_24)
         seed_bytes = Bip39SeedGenerator(mnemonic).Generate()
-        bip32_mst_ctx = Bip32Slip10Secp256k1.FromSeed(seed_bytes)
+        bip32_mst_ctx = Bip32Slip10Se256k1.FromSeed(seed_bytes)
         MasterKey = bip32_mst_ctx.PrivateKey().Raw().ToHex()
         bip32_der_ctx = bip32_mst_ctx.DerivePath("m/44'/60'/0'/0/0")
         PrivateKeyBytes = bip32_der_ctx.PrivateKey().Raw().ToHex()
         addr = EthAddrEncoder.EncodeKey(bip32_der_ctx.PublicKey().KeyObject())
-        Words24 = str(mnemonic)
+        Words24 = str(mnemoni
 
         if addr in add:
             fu += 1
-            print(f"[green1][+] MATCH ADDRESS FOUND IN LIST IMPORTED :[/green1] [white]{addr}[/white]")
+            print(f"[green1][+] MATCH ADDRESS FOUND IN LIST IMPORT :[/green1] [white]{addr}[/white]")
             print(
                 f"PrivateKey (Byte) : [green1]{PrivateKeyBytes}[/green1]\n[gold1]{mnemonic}[/gold1]\n[red1]MasterKey (Byte) : [/red1][green1]{MasterKey}[/green1]")
             with open('FoundMATCHAddr.txt', 'a') as f:
@@ -56,11 +56,11 @@ def Main():
         elif int(z) % int(logpx) == 0:
             logp += int(logpx)
             print(
-                f"[red][[green1]+[/green1]][GENERATED[white] {logp}[/white] ETH ADDR][WITH [white]128 THREAD[/white]][sK/Th:[white]{time.thread_time()}[/white]][/red]\n[red][[green1]{PrivateKeyBytes.upper()}[/green1]][/red]")
+                f"[red][[green1]+[/green1]][GENERATED[whi] {logp}[/white] ETH ADDR][WITH [white]128 THREAD[/white]][sK/Th:[white]{time.thread_time()}[/white]][/red]\n[red][[green1]{PrivateKeyBytes.upper()}[/green1]][/red]")
             print(
                 f"[red][MasterKey : [white]{MasterKey.upper()}[/white]][/red]\n[white on red3][MNEMONIC : {Words24[0:64]}...][/white on red3]")
         else:
-            print(
+            prin
                 f"[red][-][ GENERATED [cyan]{z}[/cyan] ETH ADDR ][FOUND:[white]{fu}[/white]][THREAD:[cyan]{thco}[/cyan]][/red]",
                 end="\r")
 
